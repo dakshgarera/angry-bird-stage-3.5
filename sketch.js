a8e3833ff06f64aafc36ddcd32ddfd732caf3b46
dakshgarera <dakshgarera19@gmail.com>
2021-04-12 13:58:30 +0530

    constraneOBject

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var constranelot;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -15,7 +17,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-
+constranelot = new Log(230,180,80,PI/2);
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
@@ -35,6 +37,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    chain = new Chain(bird.body,constranelot.body)
 
 }
 
@@ -59,6 +62,14 @@ function draw(){
     log4.display();
     log5.display();
 
+
+    constranelot.display();
+
     bird.display();
     platform.display();
+    chain.display();
 }
+
+
+
+
